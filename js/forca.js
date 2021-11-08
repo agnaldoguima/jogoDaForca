fcc67e87370868dceff329c4f97a8fb5ec0786f9
Agnaldo Guimaraes <tec.agnaldo@gmail.com>
2021-11-08 15:09:18 -0300

    monta a quantidade de letras na tela referente a palavra secreta

---
+++ js/forca.js
@@ -0,0 +1,244 @@
+
+let listaDinamica = [];
+let palavraSecretaCategoria;
+let palavraSecretaSorteada;
+const palavras = [
+    palavra001 = {
+        nome: "IRLANDA",
+        categoria:"LUGARES"
+    },
+    palavra002 = {
+        nome: "EQUADOR",
+        categoria:"LUGARES"
+    },
+    palavra003 = {
+        nome: "CHILE",
+        categoria:"LUGARES"
+    },
+    palavra004 = {
+        nome: "INDONESIA",
+        categoria:"LUGARES"
+    },
+    palavra005 = {
+        nome: "MALDIVAS",
+        categoria:"LUGARES"
+    },
+    palavra006 = {
+        nome: "INGLATERRA",
+        categoria:"LUGARES"
+    },
+    palavra007 = {
+        nome: "GROELANDIA",
+        categoria:"LUGARES"
+    },
+    palavra008 = {
+        nome: "UZBEQUISTAO",
+        categoria:"LUGARES"
+    },
+    palavra009 = {
+        nome: "INDONESIA",
+        categoria:"LUGARES"
+    },
+    palavra010 = {
+        nome: "CREGUENHEM",
+        categoria:"LUGARES"
+    },
+    palavra011 = {
+        nome: "BICICLETA",
+        categoria:"TRANSPORTE"
+    },
+    palavra012 = {
+        nome: "LANCHA",
+        categoria:"TRANSPORTE"
+    },
+    palavra013 = {
+        nome: "NAVIO",
+        categoria:"TRANSPORTE"
+    },
+    palavra014 = {
+        nome: "TELEFERICO",
+        categoria:"TRANSPORTE"
+    },
+    palavra015 = {
+        nome: "MOTOCICLETA",
+        categoria:"TRANSPORTE"
+    },
+    palavra016 = {
+        nome: "BARCO",
+        categoria:"TRANSPORTE"
+    },
+    palavra017 = {
+        nome: "AERONAVE",
+        categoria:"TRANSPORTE"
+    },
+    palavra018 = {
+        nome: "TREM",
+        categoria:"TRANSPORTE"
+    },
+    palavra019 = {
+        nome: "CAIAQUE",
+        categoria:"TRANSPORTE"
+    },
+    palavra020 = {
+        nome: "FUNICULAR",
+        categoria:"TRANSPORTE"
+    },
+    palavra021 = {
+        nome: "XICARA",
+        categoria:"OBJETOS"
+    },
+    palavra022 = {
+        nome: "MOEDA",
+        categoria:"OBJETOS"
+    },
+    palavra023 = {
+        nome: "ESPARADRAPO",
+        categoria:"OBJETOS"
+    },
+    palavra024 = {
+        nome: "SINO",
+        categoria:"OBJETOS"
+    },
+    palavra025 = {
+        nome: "CHUVEIRO",
+        categoria:"OBJETOS"
+    },
+    palavra026 = {
+        nome: "TAMBORETE",
+        categoria:"OBJETOS"
+    },
+    palavra027 = {
+        nome: "LAMPADA",
+        categoria:"OBJETOS"
+    },
+    palavra028 = {
+        nome: "BOCAL",
+        categoria:"OBJETOS"
+    },
+    palavra029 = {
+        nome: "CORTINA",
+        categoria:"OBJETOS"
+    },
+    palavra030 = {
+        nome: "LAPIS",
+        categoria:"OBJETOS"
+    },
+    palavra031 = {
+        nome: "MELANCIA",
+        categoria:"ALIMENTOS"
+    },
+    palavra032 = {
+        nome: "AMENDOIM",
+        categoria:"ALIMENTOS"
+    },
+    palavra033 = {
+        nome: "ESFIRRA",
+        categoria:"ALIMENTOS"
+    },
+    palavra034 = {
+        nome: "GOROROBA",
+        categoria:"ALIMENTOS"
+    },
+    palavra035 = {
+        nome: "JANTAR",
+        categoria:"ALIMENTOS"
+    },
+    palavra036 = {
+        nome: "SABOROSO",
+        categoria:"ALIMENTOS"
+    },
+    palavra037 = {
+        nome: "DESJEJUM",
+        categoria:"ALIMENTOS"
+    },
+    palavra038 = {
+        nome: "MASTIGAR",
+        categoria:"ALIMENTOS"
+    },
+    palavra039 = {
+        nome: "ENGOLIR",
+        categoria:"ALIMENTOS"
+    },
+    palavra040 = {
+        nome: "DOCERIA",
+        categoria:"ALIMENTOS"
+    },
+    palavra040 = {
+        nome: "DRAGAO",
+        categoria:"ANIMAIS"
+    },
+    palavra041 = {
+        nome: "GALINHA",
+        categoria:"ANIMAIS"
+    },
+    palavra042 = {
+        nome: "PAVAO",
+        categoria:"ANIMAIS"
+    },
+    palavra043 = {
+        nome: "CAMELO",
+        categoria:"ANIMAIS"
+    },
+    palavra044 = {
+        nome: "PERU",
+        categoria:"ANIMAIS"
+    },
+    palavra045 = {
+        nome: "ZEBRA",
+        categoria:"ANIMAIS"
+    },
+    palavra046 = {
+        nome: "DROMEDARIO",
+        categoria:"ANIMAIS"
+    },
+    palavra047 = {
+        nome: "CALANGO",
+        categoria:"ANIMAIS"
+    },
+    palavra048 = {
+        nome: "SAGUI",
+        categoria:"ANIMAIS"
+    },
+    palavra049 = {
+        nome: "LAGARTIXA",
+        categoria:"ANIMAIS"
+    },
+    palavra050 = {
+        nome: "HIPOPOTAMO",
+        categoria:"ANIMAIS"
+    }
+];
+
+
+criarPalavraSecreta();
+function criarPalavraSecreta(){
+    const indexPalavra = parseInt(Math.random() * palavras.length)
+    
+    palavraSecretaSorteada = palavras[indexPalavra].nome;
+    palavraSecretaCategoria = palavras[indexPalavra].categoria;
+    console.log(palavraSecretaSorteada)
+    console.log(palavraSecretaCategoria)
+}
+
+montarPalavraNaTela();
+function montarPalavraNaTela(){
+    const categoria = document.getElementById("categoria");
+    categoria.innerHTML = palavraSecretaCategoria;
+
+    const palavraTela = document.getElementById("palavra-secreta");
+    palavraTela.innerHTML = "";
+   
+    for(i = 0; i < palavraSecretaSorteada.length; i++){
+        if(listaDinamica[i] == undefined){
+            listaDinamica[i] = "&nbsp;"
+            palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+        }
+        else{
+            palavraTela.innerHTML = palavraTela.innerHTML + "<div class='letras'>" + listaDinamica[i] + "</div>"
+        }
+    }
+}
+
+
+
+
