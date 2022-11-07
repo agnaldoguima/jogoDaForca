15bc1fa880f3120eb134b2c85710400a74025c66
Agnaldo Guimaraes <tec.agnaldo@gmail.com>
2022-11-07 16:49:54 -0300

    criado a função que adiciona palavras a lista de forma manual

--- js/forca.js
+++ js/forca.js
@@ -3,253 +3,10 @@ let tentativas = 6;
 let listaDinamica = [];
 let palavraSecretaCategoria;
 let palavraSecretaSorteada;
-const palavras = [
-    palavra001 = {
-        nome: "IRLANDA",
-        categoria:"LUGARES"
-    },
-    palavra002 = {
-        nome: "EQUADOR",
-        categoria:"LUGARES"
-    },
-    palavra003 = {
-        nome: "CHILE",
-        categoria:"LUGARES"
-    },
-    palavra004 = {
-        nome: "INDONESIA",
-        categoria:"LUGARES"
-    },
-    palavra005 = {
-        nome: "MALDIVAS",
-        categoria:"LUGARES"
-    },
-    palavra006 = {
-        nome: "INGLATERRA",
-        categoria:"LUGARES"
-    },
-    palavra007 = {
-        nome: "GROELANDIA",
-        categoria:"LUGARES"
-    },
-    palavra008 = {
-        nome: "UZBEQUISTAO",
-        categoria:"LUGARES"
-    },
-    palavra009 = {
-        nome: "INDONESIA",
-        categoria:"LUGARES"
-    },
-    palavra010 = {
-        nome: "CREGUENHEM",
-        categoria:"LUGARES"
-    },
-    palavra011 = {
-        nome: "BICICLETA",
-        categoria:"TRANSPORTE"
-    },
-    palavra012 = {
-        nome: "LANCHA",
-        categoria:"TRANSPORTE"
-    },
-    palavra013 = {
-        nome: "NAVIO",
-        categoria:"TRANSPORTE"
-    },
-    palavra014 = {
-        nome: "TELEFERICO",
-        categoria:"TRANSPORTE"
-    },
-    palavra015 = {
-        nome: "MOTOCICLETA",
-        categoria:"TRANSPORTE"
-    },
-    palavra016 = {
-        nome: "BARCO",
-        categoria:"TRANSPORTE"
-    },
-    palavra017 = {
-        nome: "AERONAVE",
-        categoria:"TRANSPORTE"
-    },
-    palavra018 = {
-        nome: "TREM",
-        categoria:"TRANSPORTE"
-    },
-    palavra019 = {
-        nome: "CAIAQUE",
-        categoria:"TRANSPORTE"
-    },
-    palavra020 = {
-        nome: "FUNICULAR",
-        categoria:"TRANSPORTE"
-    },
-    palavra021 = {
-        nome: "XICARA",
-        categoria:"OBJETOS"
-    },
-    palavra022 = {
-        nome: "MOEDA",
-        categoria:"OBJETOS"
-    },
-    palavra023 = {
-        nome: "ESPARADRAPO",
-        categoria:"OBJETOS"
-    },
-    palavra024 = {
-        nome: "SINO",
-        categoria:"OBJETOS"
-    },
-    palavra025 = {
-        nome: "CHUVEIRO",
-        categoria:"OBJETOS"
-    },
-    palavra026 = {
-        nome: "TAMBORETE",
-        categoria:"OBJETOS"
-    },
-    palavra027 = {
-        nome: "LAMPADA",
-        categoria:"OBJETOS"
-    },
-    palavra028 = {
-        nome: "BOCAL",
-        categoria:"OBJETOS"
-    },
-    palavra029 = {
-        nome: "CORTINA",
-        categoria:"OBJETOS"
-    },
-    palavra030 = {
-        nome: "LAPIS",
-        categoria:"OBJETOS"
-    },
-    palavra031 = {
-        nome: "MELANCIA",
-        categoria:"ALIMENTOS"
-    },
-    palavra032 = {
-        nome: "AMENDOIM",
-        categoria:"ALIMENTOS"
-    },
-    palavra033 = {
-        nome: "ESFIRRA",
-        categoria:"ALIMENTOS"
-    },
-    palavra034 = {
-        nome: "GOROROBA",
-        categoria:"ALIMENTOS"
-    },
-    palavra035 = {
-        nome: "JANTAR",
-        categoria:"ALIMENTOS"
-    },
-    palavra036 = {
-        nome: "SABOROSO",
-        categoria:"ALIMENTOS"
-    },
-    palavra037 = {
-        nome: "DESJEJUM",
-        categoria:"ALIMENTOS"
-    },
-    palavra038 = {
-        nome: "MASTIGAR",
-        categoria:"ALIMENTOS"
-    },
-    palavra039 = {
-        nome: "ENGOLIR",
-        categoria:"ALIMENTOS"
-    },
-    palavra040 = {
-        nome: "DOCERIA",
-        categoria:"ALIMENTOS"
-    },
-    palavra040 = {
-        nome: "DRAGAO",
-        categoria:"ANIMAIS"
-    },
-    palavra041 = {
-        nome: "GALINHA",
-        categoria:"ANIMAIS"
-    },
-    palavra042 = {
-        nome: "PAVAO",
-        categoria:"ANIMAIS"
-    },
-    palavra043 = {
-        nome: "CAMELO",
-        categoria:"ANIMAIS"
-    },
-    palavra044 = {
-        nome: "PERU",
-        categoria:"ANIMAIS"
-    },
-    palavra045 = {
-        nome: "ZEBRA",
-        categoria:"ANIMAIS"
-    },
-    palavra046 = {
-        nome: "DROMEDARIO",
-        categoria:"ANIMAIS"
-    },
-    palavra047 = {
-        nome: "CALANGO",
-        categoria:"ANIMAIS"
-    },
-    palavra048 = {
-        nome: "SAGUI",
-        categoria:"ANIMAIS"
-    },
-    palavra049 = {
-        nome: "LAGARTIXA",
-        categoria:"ANIMAIS"
-    },
-    palavra050 = {
-        nome: "HIPOPOTAMO",
-        categoria:"ANIMAIS"
-    },
-    palavra051 = {
-        nome: "A ERA DO GELO",
-        categoria:"TV E CINEMA"
-    },
-    palavra052 = {
-        nome: "HOMEM ARANHA",
-        categoria:"TV E CINEMA"
-    },
-    palavra053 = {
-        nome: "CASA MONSTRO",
-        categoria:"TV E CINEMA"
-    },
-    palavra054 = {
-        nome: "TELA QUENTE",
-        categoria:"TV E CINEMA"
-    },
-    palavra055 = {
-        nome: "STRANGER THINGS",
-        categoria:"TV E CINEMA"
-    },
-    palavra056 = {
-        nome: "O REI DO GADO",
-        categoria:"TV E CINEMA"
-    },
-    palavra057 = {
-        nome: "MULHER MARAVILHA",
-        categoria:"TV E CINEMA"
-    },
-    palavra058 = {
-        nome: "O INCRIVEL HULK",
-        categoria:"TV E CINEMA"
-    },
-    palavra059 = {
-        nome: "BOB ESPONJA",
-        categoria:"TV E CINEMA"
-    },
-    palavra060 = {
-        nome: "PANICO NA TV",
-        categoria:"TV E CINEMA"
-    }
-];
+let palavras = [];
+let jogoAutomatico = true;
 
+carregaListaAutomatica();
 
 criarPalavraSecreta();
 function criarPalavraSecreta(){
@@ -410,6 +167,323 @@ bntReiniciar.addEventListener("click", function(){
     location.reload();
 });
 
+
+function listaAutomatica(){ // ativa o modo manual
+    if (jogoAutomatico == true) {
+        document.getElementById("jogarAutomatico").innerHTML = "<i class='bx bx-play-circle'></i>"
+        palavras = [];
+        jogoAutomatico = false;
+
+        document.getElementById("abreModalAddPalavra").style.display = "block";
+        document.getElementById("status").innerHTML = "Modo Manual";
+    }
+    else if(jogoAutomatico == false){ // ativa o modo automático
+        document.getElementById("jogarAutomatico").innerHTML = "<i class='bx bx-pause-circle'></i>"
+        jogoAutomatico = true;
+
+        document.getElementById("abreModalAddPalavra").style.display = "none";
+        document.getElementById("status").innerHTML = "Modo Automático";
+        
+    }
+}
+
+const modal = document.getElementById("modal-alerta");
+
+const btnAbreModal = document.getElementById("abreModalAddPalavra");
+btnAbreModal.onclick = function(){
+    modal.style.display = "block";
+}
+
+const btnFechaModal = document.getElementById("fechaModal");
+btnFechaModal.onclick = function(){ 
+    modal.style.display = "none";
+    document.getElementById("addPalavra").value = "";
+    document.getElementById("addCategoria").value = ""; 
+}
+
+window.onclick = function(){ 
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById("addPalavra").value = "";
+        document.getElementById("addCategoria").value = ""; 
+    }  
+}
+
+function carregaListaAutomatica(){
+    palavras = [
+        palavra001 = {
+            nome: "IRLANDA",
+            categoria:"LUGARES"
+        },
+        palavra002 = {
+            nome: "EQUADOR",
+            categoria:"LUGARES"
+        },
+        palavra003 = {
+            nome: "CHILE",
+            categoria:"LUGARES"
+        },
+        palavra004 = {
+            nome: "INDONESIA",
+            categoria:"LUGARES"
+        },
+        palavra005 = {
+            nome: "MALDIVAS",
+            categoria:"LUGARES"
+        },
+        palavra006 = {
+            nome: "INGLATERRA",
+            categoria:"LUGARES"
+        },
+        palavra007 = {
+            nome: "GROELANDIA",
+            categoria:"LUGARES"
+        },
+        palavra008 = {
+            nome: "UZBEQUISTAO",
+            categoria:"LUGARES"
+        },
+        palavra009 = {
+            nome: "INDONESIA",
+            categoria:"LUGARES"
+        },
+        palavra010 = {
+            nome: "CREGUENHEM",
+            categoria:"LUGARES"
+        },
+        palavra011 = {
+            nome: "BICICLETA",
+            categoria:"TRANSPORTE"
+        },
+        palavra012 = {
+            nome: "LANCHA",
+            categoria:"TRANSPORTE"
+        },
+        palavra013 = {
+            nome: "NAVIO",
+            categoria:"TRANSPORTE"
+        },
+        palavra014 = {
+            nome: "TELEFERICO",
+            categoria:"TRANSPORTE"
+        },
+        palavra015 = {
+            nome: "MOTOCICLETA",
+            categoria:"TRANSPORTE"
+        },
+        palavra016 = {
+            nome: "BARCO",
+            categoria:"TRANSPORTE"
+        },
+        palavra017 = {
+            nome: "AERONAVE",
+            categoria:"TRANSPORTE"
+        },
+        palavra018 = {
+            nome: "TREM",
+            categoria:"TRANSPORTE"
+        },
+        palavra019 = {
+            nome: "CAIAQUE",
+            categoria:"TRANSPORTE"
+        },
+        palavra020 = {
+            nome: "FUNICULAR",
+            categoria:"TRANSPORTE"
+        },
+        palavra021 = {
+            nome: "XICARA",
+            categoria:"OBJETOS"
+        },
+        palavra022 = {
+            nome: "MOEDA",
+            categoria:"OBJETOS"
+        },
+        palavra023 = {
+            nome: "ESPARADRAPO",
+            categoria:"OBJETOS"
+        },
+        palavra024 = {
+            nome: "SINO",
+            categoria:"OBJETOS"
+        },
+        palavra025 = {
+            nome: "CHUVEIRO",
+            categoria:"OBJETOS"
+        },
+        palavra026 = {
+            nome: "TAMBORETE",
+            categoria:"OBJETOS"
+        },
+        palavra027 = {
+            nome: "LAMPADA",
+            categoria:"OBJETOS"
+        },
+        palavra028 = {
+            nome: "BOCAL",
+            categoria:"OBJETOS"
+        },
+        palavra029 = {
+            nome: "CORTINA",
+            categoria:"OBJETOS"
+        },
+        palavra030 = {
+            nome: "LAPIS",
+            categoria:"OBJETOS"
+        },
+        palavra031 = {
+            nome: "MELANCIA",
+            categoria:"ALIMENTOS"
+        },
+        palavra032 = {
+            nome: "AMENDOIM",
+            categoria:"ALIMENTOS"
+        },
+        palavra033 = {
+            nome: "ESFIRRA",
+            categoria:"ALIMENTOS"
+        },
+        palavra034 = {
+            nome: "GOROROBA",
+            categoria:"ALIMENTOS"
+        },
+        palavra035 = {
+            nome: "JANTAR",
+            categoria:"ALIMENTOS"
+        },
+        palavra036 = {
+            nome: "SABOROSO",
+            categoria:"ALIMENTOS"
+        },
+        palavra037 = {
+            nome: "DESJEJUM",
+            categoria:"ALIMENTOS"
+        },
+        palavra038 = {
+            nome: "MASTIGAR",
+            categoria:"ALIMENTOS"
+        },
+        palavra039 = {
+            nome: "ENGOLIR",
+            categoria:"ALIMENTOS"
+        },
+        palavra040 = {
+            nome: "DOCERIA",
+            categoria:"ALIMENTOS"
+        },
+        palavra040 = {
+            nome: "DRAGAO",
+            categoria:"ANIMAIS"
+        },
+        palavra041 = {
+            nome: "GALINHA",
+            categoria:"ANIMAIS"
+        },
+        palavra042 = {
+            nome: "PAVAO",
+            categoria:"ANIMAIS"
+        },
+        palavra043 = {
+            nome: "CAMELO",
+            categoria:"ANIMAIS"
+        },
+        palavra044 = {
+            nome: "PERU",
+            categoria:"ANIMAIS"
+        },
+        palavra045 = {
+            nome: "ZEBRA",
+            categoria:"ANIMAIS"
+        },
+        palavra046 = {
+            nome: "DROMEDARIO",
+            categoria:"ANIMAIS"
+        },
+        palavra047 = {
+            nome: "CALANGO",
+            categoria:"ANIMAIS"
+        },
+        palavra048 = {
+            nome: "SAGUI",
+            categoria:"ANIMAIS"
+        },
+        palavra049 = {
+            nome: "LAGARTIXA",
+            categoria:"ANIMAIS"
+        },
+        palavra050 = {
+            nome: "HIPOPOTAMO",
+            categoria:"ANIMAIS"
+        },
+        palavra051 = {
+            nome: "A ERA DO GELO",
+            categoria:"TV E CINEMA"
+        },
+        palavra052 = {
+            nome: "HOMEM ARANHA",
+            categoria:"TV E CINEMA"
+        },
+        palavra053 = {
+            nome: "CASA MONSTRO",
+            categoria:"TV E CINEMA"
+        },
+        palavra054 = {
+            nome: "TELA QUENTE",
+            categoria:"TV E CINEMA"
+        },
+        palavra055 = {
+            nome: "STRANGER THINGS",
+            categoria:"TV E CINEMA"
+        },
+        palavra056 = {
+            nome: "O REI DO GADO",
+            categoria:"TV E CINEMA"
+        },
+        palavra057 = {
+            nome: "MULHER MARAVILHA",
+            categoria:"TV E CINEMA"
+        },
+        palavra058 = {
+            nome: "O INCRIVEL HULK",
+            categoria:"TV E CINEMA"
+        },
+        palavra059 = {
+            nome: "BOB ESPONJA",
+            categoria:"TV E CINEMA"
+        },
+        palavra060 = {
+            nome: "PANICO NA TV",
+            categoria:"TV E CINEMA"
+        }
+    ];
+}
+
+function adicionarPalavra(){
+    let addPalavra = document.getElementById("addPalavra").value.toUpperCase();
+    let addCategoria = document.getElementById("addCategoria").value.toUpperCase();
+
+    if (isNullOrWhiteSpace(addPalavra) || isNullOrWhiteSpace(addCategoria) || addPalavra.length < 3 || addCategoria.length < 3) {
+        abreModal("ATENÇÃO"," Palavra e/ou Categoria inválidos");
+        return;
+    }
+
+    let palavra = {
+        nome: addPalavra,
+        categoria: addCategoria
+    }
+
+    palavras.push(palavra);  
+    // sortear 
+    
+    document.getElementById("addPalavra").value = "";
+    document.getElementById("addCategoria").value = "";
+
+    console.log(palavras);
+}
+
+function isNullOrWhiteSpace(input){
+    return !input || !input.trim();
+}
 
 
 
